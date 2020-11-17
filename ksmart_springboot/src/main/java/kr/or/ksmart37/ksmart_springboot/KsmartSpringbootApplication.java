eefea0ac377338581a24be4a5cb33e846aa4244b
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2020-11-17 17:24:19 +0900

    Update KsmartSpringbootApplication.java

--- ksmart_springboot/src/main/java/kr/or/ksmart37/ksmart_springboot/KsmartSpringbootApplication.java
+++ ksmart_springboot/src/main/java/kr/or/ksmart37/ksmart_springboot/KsmartSpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KsmartSpringbootApplication {
 
 	public static void main(String[] args) {
+		System.out.println("springbootspringbootspringbootspringbootspringbootspringbootspringbootspringbootspringbootspringbootspringboot");
 		SpringApplication.run(KsmartSpringbootApplication.class, args);
 	}
 
