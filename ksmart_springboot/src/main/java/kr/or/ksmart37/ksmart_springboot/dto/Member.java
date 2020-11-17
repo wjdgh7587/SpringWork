3ca3c6b1096cf91a7b9465eaa8817549ab3331bd
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2020-11-17 17:24:21 +0900

    Create Member.java

---
+++ ksmart_springboot/src/main/java/kr/or/ksmart37/ksmart_springboot/dto/Member.java
@@ -0,0 +1,47 @@
+package kr.or.ksmart37.ksmart_springboot.dto;
+
+public class Member {
+	private String memberId;
+	private String memberPw;
+	private String memberName;
+	private String memberEmail;
+	private String memberLevel;
+	
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPw() {
+		return memberPw;
+	}
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getMemberLevel() {
+		return memberLevel;
+	}
+	public void setMemberLevel(String memberLevel) {
+		this.memberLevel = memberLevel;
+	}
+	@Override
+	public String toString() {
+		return "Member [memberId=" + memberId + ", memberPw=" + memberPw + ", memberName=" + memberName
+				+ ", memberEmail=" + memberEmail + ", memberLevel=" + memberLevel + "]";
+	}
+	
+	
+}
