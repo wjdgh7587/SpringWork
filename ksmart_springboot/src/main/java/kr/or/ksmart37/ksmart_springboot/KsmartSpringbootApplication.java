65515c96b0df587adaa93a4f61e2c92849c59aa3
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2020-11-17 11:42:46 +0900

    First Setting Spring
    Spring First setting

---
+++ ksmart_springboot/src/main/java/kr/or/ksmart37/ksmart_springboot/KsmartSpringbootApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart37.ksmart_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmartSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmartSpringbootApplication.class, args);
+	}
+
+}
