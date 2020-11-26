eca447d6a1bb05c9187c584c5c02f6bb479579b7
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2020-11-26 17:31:13 +0900

    test

--- ksmart_mybatis/src/main/java/kr/or/ksmart37/ksmart/mybatis/KsmartMybatisApplication.java
+++
@@ -1,9 +0,0 @@
-package kr.or.ksmart37.ksmart.mybatis;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class KsmartSpringbootApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(KsmartSpringbootApplication.class, args);
-	}
-}
